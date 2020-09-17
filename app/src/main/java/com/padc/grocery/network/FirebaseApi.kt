3eb2d5e6bf683edb8139ced6a3afe01edbc2d139
zawhtetnaing10 <zawgyi.gog@gmail.com>
2020-09-17 06:39:00 +0630

    pick Save Data Complete

--- app/src/main/java/com/padc/grocery/network/FirebaseApi.kt
+++ app/src/main/java/com/padc/grocery/network/FirebaseApi.kt
@@ -3,5 +3,6 @@ package com.padc.grocery.network
 import com.padc.grocery.data.vos.GroceryVO
 
 interface FirebaseApi {
-    fun getGroceries(onSuccess: (groceries: List<GroceryVO>) -> Unit, onFailure: (String) -> Unit)
+    fun getGroceries(onSuccess: (groceries: List<GroceryVO>) -> Unit, onFialure: (String) -> Unit)
+    fun addGrocery(name: String, description: String, amount: Int)
 }
