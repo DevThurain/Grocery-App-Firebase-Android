c1dd7a59ea4ed61e90e04126430db1f99030372b
thurain <thurainbowin.dev@gmail.com>
2022-12-07 16:28:19 +0630

    - update Delete data

--- app/src/main/java/com/padc/grocery/mvp/presenters/impls/MainPresenterImpl.kt
+++ app/src/main/java/com/padc/grocery/mvp/presenters/impls/MainPresenterImpl.kt
@@ -26,6 +26,7 @@ class MainPresenterImpl : MainPresenter, AbstractBasePresenter<MainView>() {
         )
     }
 
+
     override fun onTapDeleteGrocery(name: String) {
         mGroceryModel.removeGrocery(name)
     }
